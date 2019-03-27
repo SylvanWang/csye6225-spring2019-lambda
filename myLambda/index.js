4501747eed54113a477863375baa2ce47918d77d
Ranran He <he.ra@northeastern.edu>
2019-03-26 18:46:35 -0700

    fix lambda

--- myLambda/index.js
+++ myLambda/index.js
@@ -1,34 +1,33 @@
-const AWS = require('aws-sdk')
-AWS.config.update({ region: 'us-east-1' })
-const ses = new AWS.SES()
-const dynamoDB = new AWS.DynamoDB()
-const route53 = new AWS.Route53()
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-east-1' });
+const ses = new AWS.SES();
+const dynamoDB = new AWS.DynamoDB();
+const route53 = new AWS.Route53();
 
 exports.handler = (event, context) => {
-    const email = event.Records[0].Sns.Message
-
+    const email = event.Records[0].Sns.Message;
     const getItemObject = {
         TableName: 'csye6225',
         Key: {
             'id': { S: email }
         }
-    }
+    };
 
     dynamoDB.getItem(getItemObject, (err, data) => {
-        if (data.Item === undefined) {
+        if (data.Item === undefined || data.Item.ttl.N < Math.floor(Date.now() / 1000)) {
             const putItemObject = {
                 TableName: 'csye6225',
                 Item: {
                     id: { S: email },
                     token: { S: context.awsRequestId },
-                    ttl: { N: (Math.floor(Date.now() / 1000) + 60).toString() }
+                    ttl: { N: (Math.floor(Date.now() / 1000) + 1200).toString() }
                 }
-            }
-            dynamoDB.putItem(putItemObject, () => {})
-
+            };
+            dynamoDB.putItem(putItemObject, () => {});
             route53.listHostedZones({}, (err, data) => {
-                let domainName = data.HostedZones[0].Name
-                domainName = domainName.substring(0, domainName.length - 1)
+
+                let domainName = data.HostedZones[0].Name;
+                domainName = domainName.substring(0, domainName.length - 1);
                 const emailObject = {
                     Destination: {
                         ToAddresses: [email]
@@ -43,10 +42,10 @@ exports.handler = (event, context) => {
                             Data: "Reset password"
                         }
                     },
-                    Source: "reset-password@" + domainName
-                }
+                    Source: "noreply@" + domainName
+                };
                 ses.sendEmail(emailObject, () => {})
             });
         }
     })
-}
+};
